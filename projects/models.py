6634f3135132ce9e944980f8b3b59da3984a1206
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-07-03 17:49:38 +0100

    Fix PostgreSQL version and static files directory

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = CloudinaryField('image', blank=True, null=True)
+    github_link = models.URLField()
+    live_link = models.URLField(blank=True, null=True)
+    technologies = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class CV(models.Model):
+    resume = CloudinaryField('resume')
+
+    def __str__(self):
+        return "CV"
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Project, CV
 
-# Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    cv = CV.objects.first()
+    return render(request, 'projects/home.html', {'projects': projects, 'cv': cv})
