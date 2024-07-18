73c66acbc95d7d20a7bfce9eef5984899341a026
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-07-18 21:59:31 +0100

    Fix formatting on full_Stack

--- projects/views.py
+++ projects/views.py
@@ -24,4 +24,3 @@ def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
     template = f"projects/project_detail_{project.template}.html"
     return render(request, template, {'project': project})
-
