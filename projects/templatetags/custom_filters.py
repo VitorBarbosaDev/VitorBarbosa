385b4670324c828f0327167d7b705d2e1859ac11
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2024-07-29 20:16:31 +0100

    Add gif support almost

--- projects/templatetags/custom_filters.py
+++ projects/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
+import re
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -9,7 +11,31 @@ def truncate_words(value, word_limit):
         return ' '.join(words[:word_limit]) + '...'
     return value
 
-
 @register.filter
 def youtube_embed(url):
     return url.replace('watch?v=', 'embed/')
+
+
+@register.filter(name='embed_gifs')
+def embed_gifs(value):
+    # Find all GIF URLs in the text
+    gif_urls = re.findall(r'(https?://media\.giphy\.com/media/\S+\.gif)', value)
+
+    for url in gif_urls:
+        img_tag = f'<img src="{url}" alt="GIF" class="img-fluid mb-3 gifs">'
+
+        # Replace URLs within <a> tags first
+        value = re.sub(
+            rf'(<a[^>]*href="{re.escape(url)}"[^>]*>)(.*?)(</a>)',
+            rf'\1{img_tag}<span class="hidden-url">\2</span>\3',
+            value
+        )
+
+        # Then replace standalone URLs
+        value = re.sub(
+            rf'\b{re.escape(url)}\b',
+            f'<a href="{url}" class="gif-link">{img_tag}</a>',
+            value
+        )
+
+    return mark_safe(value)
